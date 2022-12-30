7a2dfbaa5e1a434fbc22f3e769e3b48c31ddfb41
Grogodems <121344231+Grogodems@users.noreply.github.com>
2022-12-30 03:05:35 +0300

    Add files via upload

---
+++ Source5.cpp
@@ -0,0 +1,9 @@
+class Derived : [virtual] [access - specifier] Base
+{
+	// member list
+};
+class Derived : [virtual] [access - specifier] Base1,
+	[virtual][access - specifier] Base2, . . .
+{
+	// member list
+};
