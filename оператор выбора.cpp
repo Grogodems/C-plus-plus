500dda56cb6d1a5a12e4cbc6b4e3158b88bb53cb
Grogodems <121344231+Grogodems@users.noreply.github.com>
2023-02-20 20:59:01 +0300

    Add files via upload

---
+++ оператор выбора.cpp
@@ -0,0 +1,29 @@
+int day;
+bool fDayOff;
+
+day = 3;
+
+switch (day)
+{
+case 1:
+    fDayOff = false;
+    break;
+case 2:
+    fDayOff = false;
+    break;
+case 3:
+    fDayOff = false;
+    break;
+case 4:
+    fDayOff = false;
+    break;
+case 5:
+    fDayOff = false;
+    break;
+case 6:
+    fDayOff = true;
+    break;
+case 7:
+    fDayOff = true;
+    break;
+}
